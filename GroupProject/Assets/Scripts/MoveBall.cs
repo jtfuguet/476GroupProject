cf13217520673dcff35fd1ada1554dd0b6facf9a
jtfuguet <70173007+jtfuguet@users.noreply.github.com>
2020-10-23 23:35:51 -0400

    Added script
    Move Ball

---
+++ GroupProject/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(Vector3.forward * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.AddForce(Vector3.back * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }
+    }
+}
